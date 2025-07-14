6207ee93a2124eaa9879e7cbcda734124dfb459f
Renan <desouzarenarenarena@gmail.com>
2025-07-14 15:55:05 -0300

    update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/Library-ui/refs/heads/main/Redzhubui"))()
 
 local Window = redzlib:MakeWindow({
     Title = "Tetra4 🛠️",
